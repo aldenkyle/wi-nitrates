8716d1c05aa140097705e0109c86bdbcff1d152c
Kyle Alden <aldenkyle@users.noreply.github.com>
2023-09-23 14:22:58 -0400

    figured out interpolation, and aggregation, improved viz

--- js/main.js
+++ js/main.js
@@ -17,58 +17,81 @@ var Stamen_TonerLite = L.tileLayer(
   }
 ).addTo(map);
 
-var myGeoJson;
 
-var leafletLayers;
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
 
-var geojsonMarkerOptions = {
-  color: "green"
+
+var baseMaps = {
+    "Stamen Toner Lite": Stamen_TonerLite,
+    "OpenStreetMap.HOT": osmHOT
 };
 
+
+
+var overlayMaps = {
+};
+
+var layerControl;
+var layerControl = L.control.layers(baseMaps, overlayMaps, {position: 'bottomright'}).addTo(map);
+var nitrateLayer;
+var myGeoJson;
+
+var tractLayer;
+
+function getNitrateColor(d) {
+    return d >10 ? '#014636' :
+           d > 7.5 ? '#016c59':
+           d > 5  ? '#02818a' :
+           d > 4  ? '#3690c0' :
+           d > 3  ? '#67a9cf' :
+           d > 2   ? '#a6bddb' :
+           d > 1   ? '#d0d1e6' :
+           d > 0   ? '#ece2f0' :
+                      '#fff7fb';
+}
+
+
+
+function cancerColor(d) {
+    return d >.8 ? '#7f0000' :
+           d >.65 ? '#b30000' :
+           d >.5  ? '#d7301f' :
+           d > .4  ? '#ef6548' :
+           d > .3  ? '#fc8d59' :
+           d > .2   ? '#fdbb84' :
+           d > .1   ? '#fdd49e' :
+           d > 0   ? '#fee8c8' :
+                      '#fff7ec';
+}
+
+function style0(feature) {
+    return {
+        fillColor: cancerColor(feature.properties.canrate),
+        weight: .7,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.7
+    };
+}
+
+
 var url =
-  "https://raw.githubusercontent.com/aldenkyle/wi-nitrates/main/data/well_nitrate.geojson";
+  "https://raw.githubusercontent.com/aldenkyle/wi-nitrates/main/data/cancer_tracts.json";
 
 $.ajax(url).done(function (data) {
-  myGeoJson = JSON.parse(data);
-  leafletLayers = L.geoJson(myGeoJson, {
-    pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
-  });
-  leafletLayers.addTo(map);
+  tracts = JSON.parse(data);
+  tractLayer = L.geoJson(tracts, {style:style0});
+  tractLayer.addTo(map);
+  layerControl.addOverlay(tractLayer, "Cancer Rate by Census Tract");
+  tractLayer.bringToBack()
 });
 
 
-var styleMarkers = function (closestPoints) {
-  var artIds = _.map(closestPoints, function (point) {
-    return point.properties.nitr_ran;
-  });
-  _.each(leafletLayers._layers, function (layer, index) {
-    if (_.contains(artIds, layer.feature.properties.nitr_ran)) {
-      layer.setStyle({ color: "orange", radius: 12 });
-    } else {
-      layer.setStyle({ color: "green", radius: 10 });
-    }
-  });
-};
-
-
-
-
-function getColor(d) {
-    return d >10 ? '#800026' :
-           d > 5  ? '#BD0026' :
-           d > 4  ? '#E31A1C' :
-           d > 3  ? '#FC4E2A' :
-           d > 2   ? '#FD8D3C' :
-           d > 1   ? '#FEB24C' :
-           d > 0   ? '#FED976' :
-                      '#FFEDA0';
-}
-
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.nitr_ran),
+        fillColor: getNitrateColor(feature.properties.nitr_ran),
         weight: 0,
         opacity: 1,
         //color: 'white',
@@ -78,21 +101,99 @@ function style(feature) {
 }
 
 
+
+//add nitrate well locations, styled
+
+var url =
+  "https://raw.githubusercontent.com/aldenkyle/wi-nitrates/main/data/well_nitrate.geojson";
+
+$.ajax(url).done(function (data) {
+  myGeoJson = JSON.parse(data);
+  nirtrateLayer = L.geoJson(myGeoJson, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, geoJsonMakerOptions = {
+              fillColor: getNitrateColor(feature.properties.nitr_ran),
+      weight: 0,
+      radius:4,
+      fillOpacity:1,
+      opacity:1});
+    }
+  });
+  nirtrateLayer.addTo(map);
+  layerControl.addOverlay(nirtrateLayer, "Nitrate Concentrations")
+  tractLayer.bringToBack()
+});
+
+
+
+function style2(feature) {
+    return {
+        fillColor: getNitrateColor(feature.properties.mean),
+        weight: .7,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.7
+    };
+}
+
+
+
+
+//set up function to run the analysis
 var getInterpolatedPoints = function (e) {
   var allJson = _.clone(myGeoJson);
-   // jsnBuffer= turf.buffer(leafletLayers.toGeoJSON(),20,{units:'kilometers'})
-    //lyrBuffer = L.geoJSON(jsnBuffer, {style:{color:'yellow', dashArray:'5,5', fillOpacity:0}}).addTo()
-  //var points = turf.points(allJson);
-  var options = {gridType: 'square', property: 'nitr_ran', units: 'miles', weight: 2};
-  var grid = turf.interpolate(leafletLayers.toGeoJSON(), 5, options);
-  lyrBuffer = L.geoJSON(grid, {style:style}).addTo(map)
-  //return grid;
-    console.log(grid);
+   // get IDW exponent from app
+  var idw_weight = Number(document.getElementById('idwFactor').value)
+  //console.log(idw_weight)
+  var idw_dist_map = 3
+  var idw_dist_points = 3
+  var options = {gridType: 'square', property: 'nitr_ran', units: 'miles', weight: idw_weight};
+  var grid = turf.interpolate(nirtrateLayer.toGeoJSON(), idw_dist_map, options);
+  lyrBuffer = L.geoJSON(grid, {style:style}).addTo(map);
+  layerControl.addOverlay(lyrBuffer, "Interpolated Nitrate Concentrations")
+  var ptoptions = {gridType: 'point', property: 'nitr_ran', units: 'miles', weight: idw_weight};
+  var grid_pts = turf.interpolate(nirtrateLayer.toGeoJSON(), idw_dist_points, ptoptions);
+  //console.log(grid_pts) ;
+  var collectCncr =  turf.collect(tractLayer.toGeoJSON(),grid_pts, 'nitr_ran', 'values');
+ 
+  // finds the mean of the grid points in each tract, if no points in a tract, use the value of the closest point to the centroid
+  collectCncr.features.forEach(function (feature) {
+    try {feature.properties.mean = ss.mean(feature.properties.values);
+        } catch(error) {
+      //console.error(error)
+        var centroid = turf.centroid(feature);
+        var nearestPt = turf.nearestPoint(centroid, grid_pts)
+        //console.log(nearestPt)
+        feature.properties.mean = nearestPt.properties.nitr_ran
+  }
+        });
+  //console.log(collectCncr);
+    //add layer to the map showing the nitrate rates in each tract to check it out
+    
+    lyrNitrates =  L.geoJSON(collectCncr, {style:style2}).addTo(map);
+    layerControl.addOverlay(lyrNitrates, "Nitrate Concentrations per Census Tract")
+    nirtrateLayer.bringToFront()
+    
+    //use simplestatistics to run regression
+    
+    //get r squared and add it to map
+    
+    // add a layer that shows the distribution of residuals
 };
 
 
 
+
+
+
 $("#interpolate").click(function(e){
+        try { map.removeLayer(lyrBuffer);
+            } catch(error)  {
+              console.error(error);
+              // Expected output: ReferenceError: nonExistentFunction is not defined
+              // (Note: the exact output may be browser-dependent)
+            }
+
         var closestPoints = getInterpolatedPoints(e)
     });
 
